9d866ee80f4a5e5d61c7bdb51cf3f28b47b57f1b
unknown <igornicolas33@gmail.com>
2020-12-01 21:00:14 -0300

    versao 1

---
+++ tp01web2/Book.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace tp01web2
+{
+    class Book
+    {
+        
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Author[] Authors { get; set; }
+        public double Price { get; set; }
+        public int Qty { get; set; }
+
+        public Book(string name,Author[] author,double price)
+        {
+            Name = name;
+            Authors = author;
+            Price = price;
+            Qty = 0;
+           
+        }
+        public Book(string name, Author[] author, double price,int qty)
+        {
+            Name = name;
+            Authors = author;
+            Price = price;
+            Qty = qty;
+        }
+
+        public override string ToString()
+        {
+
+            string authors = "";
+
+            foreach (Author a in Authors)
+            {
+                authors = authors+$"[name={a.Name},email={a.Email},gender={a.Gender}]";
+
+                if (a == Authors.Last())
+                {
+                }
+
+                else
+                {
+                    authors += ",";
+                }
+            }
+  
+            string stringFinal = "Book[" + Name + ",authors={Author"+ authors + "},price="+ Price + ",qty=" + Qty + "]";
+
+            return stringFinal;
+        }
+        public string getAuthorNames()
+        {
+            string author = "";
+            
+            foreach (Author a in Authors)
+            {
+                author += a.Name;
+
+                if (a == Authors.Last())
+                {
+                }
+
+                else
+                {
+                    author += ",";
+                }
+
+            }
+            return author;
+                  
+
+        }
+    }
+}
---
+++ tp01web2/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Threading;
+using tp01web2;
+
+namespace tp01web2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebHost host = new WebHostBuilder()
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .Build();
+            host.Run();
+
+            Author[] at = new Author[2];
+            Author a = new Author("Autor 1", "Autor1@gmail.com", 'f');
+            Author a1 = new Author("Autora 2", "Autora2@gmail.com", 'f');
+            at.SetValue(a, 0);
+            at.SetValue(a1, 1);
+            Book b = new Book("Livro Teste",at,25.00,2);
+            Console.WriteLine(b.ToString());
+            Console.WriteLine();
+            Console.WriteLine(b.getAuthorNames());            
+
+            Console.ReadKey();
+        }
+    }
+}
